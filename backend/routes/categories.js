d874429262b4a7b67f93b69cf43731860aba13fd
Adam Ripley <aripley@wharton.upenn.edu>
2019-11-17 20:24:48 -0500

    edit category route

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -3,12 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const Category = require('../models/category');
-const User = require('../models/user');
 const UserCheck = require('../utils/authChecking');
 
 module.exports = () => {
   /*
-  Route to handle posting a new category
+  Route to handle posting a new habit category
   */
   router.post('/add', (req, res) => {
     // Check to see if user is logged in
@@ -65,6 +64,59 @@ module.exports = () => {
             res.send({
               success: false,
               error: 'Error adding category.',
+            });
+          });
+      });
+    });
+  });
+
+  /*
+  Route to handle editing a habit category (i.e. changing color or name)
+  */
+  router.post('/edit', (req, res) => {
+    // Check to make sure user is logged in
+    UserCheck(req, (authRes) => {
+      if (!authRes.success) {
+        res.send({
+          success: false,
+          error: authRes.error,
+        });
+        return;
+      }
+      // Check to make sure params are provided (color, name)
+      const { color, name } = req.body;
+      if (!name || !color) {
+        res.send({
+          success: false,
+          error: 'Provide a name and a color.',
+        });
+        return;
+      }
+      const userId = req.session.passport.user;
+      // Search by userId and category name
+      Category.findOne({ userId, name }, (err, category) => {
+        // If it doesn't exist, throw error
+        if (err || !category) {
+          res.send({
+            success: false,
+            error: 'Error updating category.',
+          });
+          return;
+        }
+        // If it does exist, make changes to category and return
+        category.name = name;
+        category.color = color;
+        category.save()
+          .then(() => {
+            res.send({
+              success: true,
+              error: false,
+            });
+          })
+          .catch(() => {
+            res.send({
+              success: false,
+              error: 'Error updating category.',
             });
           });
       });
