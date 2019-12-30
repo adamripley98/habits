7f05ed2d56409b7b32249b964ced6e8147c09fa5
Adam Ripley <aripley@wharton.upenn.edu>
2019-12-29 15:51:41 -0900

    basic info section

--- client/src/pages/Home/AccountSettings.js
+++ client/src/pages/Home/AccountSettings.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import {
-  loadRelationships, addFriend, acceptFriend, rejectFriend,
-} from '../../redux/actions/friends';
 import SideNav from '../../components/SideNav';
 import Loading from '../../components/shared/Loading';
 import ErrorMessage from '../../components/shared/ErrorMessage';
@@ -21,22 +18,100 @@ class AccountSettings extends Component {
     });
   }
 
+  showBasicSettings() {
+    return (
+      <div className="category-card p-3">
+        <h3 className="card-title">Basic Information</h3>
+        <div className="line" />
+        <div className="setting-section">
+          <div>
+            <p className="bold mb-0">Full Name</p>
+            <p className="mb-2">Adam Ripley</p>
+          </div>
+          <div className="edit-link">
+            <i className="far fa-edit" />
+            <p>Edit</p>
+          </div>
+        </div>
+        <div className="line" />
+        <div className="setting-section">
+          <div>
+            <p className="bold mb-0">Password</p>
+            <p className="mb-2">&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;</p>
+          </div>
+          <div className="edit-link">
+            <i className="far fa-edit" />
+            <p>Edit</p>
+          </div>
+        </div>
+        <div className="line" />
+        <div className="setting-section">
+          <div>
+            <p className="bold mb-0">Profile Picture</p>
+            <img src={Adam} alt="change-pic" className="change-pic" />
+          </div>
+          <div className="edit-link mt-4">
+            <i className="far fa-edit" />
+            <p>Edit</p>
+          </div>
+        </div>
+        <div className="line" />
+        <div className="setting-section">
+          <div>
+            <p className="bold mb-0">Location</p>
+            <p className="mb-2">Anchorage, AK</p>
+          </div>
+          <div className="edit-link">
+            <i className="far fa-edit" />
+            <p>Edit</p>
+          </div>
+        </div>
+        <div className="line" />
+        <div className="setting-section">
+          <div>
+            <p className="bold mb-0">Bio</p>
+            <p className="mb-2">Hello I'm a lifting bro from AK and i finna meet gurlz</p>
+          </div>
+          <div className="edit-link">
+            <i className="far fa-edit" />
+            <p>Edit</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  showNotificationSettings() {
+    return (
+      <div className="category-card p-3">
+        <h3 className="navy-text bold pb-2 pl-3 pt-1">Notification Preferences</h3>
+        <div className="line" />
+      </div>
+    );
+  }
+
+  showPrivacySettings() {
+    return (
+      <div className="category-card p-3">
+        <h3 className="navy-text bold pb-2 pl-3 pt-1">Privacy Settings</h3>
+        <div className="line" />
+      </div>
+    );
+  }
+
   showContent() {
     return (
       <div>
         <ErrorMessage error={this.props.error} />
         <div className="row">
           <div className="col-lg-6">
-            <h1>hello</h1>
+            {this.showBasicSettings()}
           </div>
           <div className="col-lg-6">
-            <h1>hello</h1>
+            {this.showNotificationSettings()}
           </div>
           <div className="col-lg-6">
-            <h1>hello</h1>
-          </div>
-          <div className="col-lg-6">
-            <h1>hello</h1>
+            {this.showPrivacySettings()}
           </div>
         </div>
       </div>
